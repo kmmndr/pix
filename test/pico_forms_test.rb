3060b985ea653072797dedc3bb77212284302153
Thomas Kienlen <kommander@laposte.net>
2023-09-11 21:55:16 +0200

    ✅ Add tests for basic forms

--- test/pico_forms_test.rb
+++ test/pico_forms_test.rb
@@ -5,7 +5,22 @@ class PicoFormsTest < Minitest::Test
   include Phlex::Testing::ViewHelper
 
   def test_pico_basic_inputs
-    output = render Pix::PicoForms::PicoInputText.new(placeholder: 'foo')
-    assert_equal '<input type="text" placeholder="foo" aria-label="foo">', output
+    output = render Pix::PicoForms::PicoInputText.new(placeholder: 'Text')
+    assert_equal '<input type="text" placeholder="Text" aria-label="Text">', output
+
+    output = render Pix::PicoForms::PicoInputEmail.new(placeholder: 'Email')
+    assert_equal '<input type="email" placeholder="Email" aria-label="Email">', output
+
+    output = render Pix::PicoForms::PicoInputNumber.new(placeholder: 'Number')
+    assert_equal '<input type="number" placeholder="Number" aria-label="Number">', output
+
+    output = render Pix::PicoForms::PicoInputPassword.new(placeholder: 'Password')
+    assert_equal '<input type="password" placeholder="Password" aria-label="Password">', output
+
+    output = render Pix::PicoForms::PicoInputTel.new(placeholder: 'Tel')
+    assert_equal '<input type="tel" placeholder="Tel" aria-label="Tel">', output
+
+    output = render Pix::PicoForms::PicoInputUrl.new(placeholder: 'Url')
+    assert_equal '<input type="url" placeholder="Url" aria-label="Url">', output
   end
 end
